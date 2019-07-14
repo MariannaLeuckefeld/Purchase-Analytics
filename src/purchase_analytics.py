1eac04d3d5ee12547e595b32599572d50ce707e1
Marianna Leuckefeld <leuckefeld@googlemail.com>
2019-07-13 21:12:04 -0700

    added question2 and percentage count. Created final dictionary

--- src/purchase_analytics.py
+++ src/purchase_analytics.py
@@ -33,6 +33,7 @@ def get_input(path):
             res.append(row)
     return res
 
+#merge together based on product_id
 def merge(orders, products):
     res=[]
     for row in orders:
@@ -42,14 +43,49 @@ def merge(orders, products):
                 res.append(batch_dict)
     return res
 
+#calculate the number_of_orders greater than 0
 
+def number_of_orders(results):
+    res = {}
+    department_ids = [r['department_id'] for r in results]
+    for department_id in department_ids:
+        res[department_id] = 0
+    for row in results:
+        res[row['department_id']] += 1
+    return res
+
+
+#calculate the number_of_first_orders
+def number_of_first_orders(results):
+     res = {}
+     department_ids = [r['department_id'] for r in results]
+     for department_id in department_ids:
+         res[department_id] = 0
+     for row in results:
+         if row['reordered'] == '0':
+             res[row['department_id']] +=1
+     return res
+
+
+
+# calculate percentage (number_of_first_orders divided by number_of_orders)
+def percentage(q1,q2):
+    res=[]
+    for id in q1.keys():
+        res.append({'department_id':id,'number_of_orders':q1[id],'number_of_first_orders':q2[id],'percentage':(q2[id]/q1[id])})
+    return res
 
 
 
 
 
 # creation and writing into the final csv file
-def output():
+def output(q1):
+    result=[]
+    result.append(q1)
+
+
+
     with open(args.report_path,'w') as fh_report:
         wr =csv.writer(fh_report, quoting=csv.QUOTE_ALL)
      #   wr.writerow()
@@ -61,6 +97,11 @@ def output():
 if __name__=='__main__':
     orders = get_input(args.order_products_path)
     products = get_input(args.products_path)
-    output()
     results = merge(orders,products)
     print(results)
+    q1 = number_of_orders(results)
+    print(q1)
+    q2 = number_of_first_orders(results)
+    print(q2)
+    q3 = percentage(q1,q2)
+    print(q3)
