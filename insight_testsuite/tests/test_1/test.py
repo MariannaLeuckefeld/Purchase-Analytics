08ce4a7e8e48ba4914d41564707ff1c39c8131f6
Marianna Leuckefeld <leuckefeld@googlemail.com>
2019-07-14 19:47:39 -0700

    worked on descriptions and fixed declaration for test.py

--- insight_testsuite/tests/test_1/test.py
+++ insight_testsuite/tests/test_1/test.py
@@ -5,17 +5,15 @@ and the test_output is the same as the source_of truth output
 
 import unittest
 import subprocess
+import os
 
 
 class TestPurchaseAnalytics(unittest.TestCase):
     def test_script(self):
         """calls purchase_analytics.py on test files"""
         subprocess.check_call(['python', '../../../src/purchase_analytics.py', 'input/order_products.csv',
-                               'input/products.csv', 'output/report_generated.csv'])
-
-    def test_cvs(self):
-        """ tests if the generated test_file is the same like the (source of truth) output file"""
-        with open('output/report.csv', 'r') as file1, open('output/report_generated.csv', 'r') as file2:
+                               'input/products.csv', self.output_file])
+        with open('output/report.csv', 'r') as file1, open(self.output_file, 'r') as file2:
             truth = file1.readline()
             compare = file2.readline()
 
@@ -24,6 +22,13 @@ class TestPurchaseAnalytics(unittest.TestCase):
 
         for row in compare:
             self.assertTrue(row in truth)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.output_file = 'output/report_generated.csv'
+        exists = os.path.isfile(cls.output_file)
+        if exists:
+            os.remove(cls.output_file)
 
 
 if __name__ == "__main__":
--- src/purchase_analytics.py
+++ src/purchase_analytics.py
@@ -1,5 +1,15 @@
 """
 Script to gather the input files and create an output file as report.csv
+
+q1 = implies output of question 1
+    number_of_orders. How many times was a product requested from this department?
+    (If the same product was ordered multiple times, we count it as multiple requests)
+q2 = implies output of question 2
+    number_of_first_orders. How many of those requests contain products ordered for the first time?
+q3 implies output of question 3
+    percentage. What is the percentage of requests containing products ordered for the first time compared with the
+    total number of requests for products from that department?
+    (e.g., number_of_first_orders divided by number_of_orders)
 """
 
 import csv
@@ -24,7 +34,7 @@ def get_input(path):
 
 def merge(orders, products):
     """merge together based on product_id"""
-    res=[]
+    res = []
     for row in orders:
         for row1 in products:
             if row['product_id'] == row1['product_id']:
@@ -46,22 +56,23 @@ def number_of_orders(results):
 
 def number_of_first_orders(results):
     """calculate the number_of_first_orders (when reordered ==0) for every department ID"""
-     res = {}
-     department_ids = [r['department_id'] for r in results]
-     for department_id in department_ids:
-         res[department_id] = 0
-     for row in results:
-         if row['reordered'] == '0':
-             res[row['department_id']] +=1
-     return res
+    res = {}
+    department_ids = [r['department_id'] for r in results]
+    for department_id in department_ids:
+        res[department_id] = 0
+    for row in results:
+        if row['reordered'] == '0':
+            res[row['department_id']] += 1
+    return res
 
 
-def percentage(q1,q2):
+def percentage(q1, q2):
     """# We are creating one output and merging q1 and q2 based on the department ID together
       # calculate percentage (number_of_first_orders divided by number_of_orders)"""
-    res=[]
+    res = []
     for id in q1.keys():
-        res.append({'department_id':id,'number_of_orders':q1[id],'number_of_first_orders':q2[id],'percentage':(q2[id]/q1[id])})
+        res.append({'department_id': id, 'number_of_orders': q1[id], 'number_of_first_orders': q2[id],
+                    'percentage': (q2[id]/q1[id])})
     return res
 
 
@@ -74,11 +85,11 @@ def output(q3):
             writer.writerow(data)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     orders = get_input(args.order_products_path)
     products = get_input(args.products_path)
-    results = merge(orders,products)
+    results = merge(orders, products)
     q1 = number_of_orders(results)
     q2 = number_of_first_orders(results)
-    q3 = percentage(q1,q2)
+    q3 = percentage(q1, q2)
     output(q3)
